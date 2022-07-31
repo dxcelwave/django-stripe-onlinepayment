6f4fb6de30950a2b45acd2a35b04f6e4ee1b76ba
kosei <higashi.k24@gmail.com>
2022-07-31 09:34:59 +0900

    add new file

---
+++ Project_Folder/urls.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+from django.urls import path
+from django.conf import settings            # settings.pyの変数
+from django.conf.urls.static import static  # メディア表示
+
+# App_Folderからviews.pyで定義した関数呼出
+from App_Folder.views import (
+    CreateCheckoutSessionView,
+    ProductTopPageView,
+    SuccessPageView,
+    CancelPageView,
+    stripe_webhook,
+    )
+
+urlpatterns = [
+    path('admin/', admin.site.urls),                                                                               # 管理画面
+    path("", ProductTopPageView.as_view(), name="product-top-page"),                                               # 商品トップ
+    path("create-checkout-session/<pk>/", CreateCheckoutSessionView.as_view(), name="create-checkout-session"),    # 個別商品決済画面
+    path("success/", SuccessPageView.as_view(), name="success"),                                                   # 決済成功時にリダイレクト先
+    path("cancel/", CancelPageView.as_view(), name="cancel"),                                                      # 決済キャンセル時のリダイレクト先
+    path("webhook/", stripe_webhook, name="webhook"),                                                              # 追加 Webhook
+]
+
+# メディア表示
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
